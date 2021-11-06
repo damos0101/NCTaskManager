c30c93e20822f711f1eb359119250e7198ca6f48
Danil Moskvin <62033064+damos0101@users.noreply.github.com>
2021-11-06 13:20:16 +0200

    new class ArrayTaskList commit

---
+++ src/main/java/ua/edu/sumdu/j2se/moskvin/tasks/ArrayTaskList.java
@@ -0,0 +1,48 @@
+package ua.edu.sumdu.j2se.moskvin.tasks;
+
+public class ArrayTaskList {
+    private int size = 0;
+    Task[] tasks = new Task[size];
+
+    public void add(Task task) {
+        Task[] newTasks = new Task[size + 1];
+        for (int i = 0; i < size; ++i) {
+            newTasks[i] = tasks[i];
+        }
+        tasks = newTasks;
+        ++size;
+        tasks[size - 1] = task;
+    }
+
+    public boolean remove(Task task) {
+        for (int i = 0; i < size; ++i) {
+            if (task.getTitle() == tasks[i].getTitle()) {
+                --size;
+                tasks[i] = null;
+                for (int j = i; j < size; ++j) {
+                    tasks[j] = tasks[j + 1];
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public Task getTask(int index) {
+        return tasks[index];
+    }
+
+    public ArrayTaskList incoming(int from, int to) {
+        ArrayTaskList newTaskList = new ArrayTaskList();
+        for (int i = 0; i < size; ++i) {
+            if (tasks[i].getStartTime() > from && tasks[i].getEndTime() < to && tasks[i].isActive()) {
+                newTaskList.add(tasks[i]);
+            }
+        }
+        return newTaskList;
+    }
+}
